e2f044be2de92b44628782d957bc923e92199760
loopbackfailed <121802508+loopbackfailed@users.noreply.github.com>
2023-06-10 00:19:22 +0530

    Create animation1.cpp
    a

---
+++ animation1.cpp
@@ -0,0 +1,117 @@
+#include <GL/glut.h>
+
+#define ESCAPE 27
+
+int window;
+float angle = 0.0f;
+
+void InitGL(int Width, int Height)
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Set window color
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f, (GLfloat)Width / (GLfloat)Height, 0.1f, 100.0f);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void DrawCube()
+{
+    // Front face (red)
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glEnd();
+
+    // Back face (green)
+    glBegin(GL_QUADS);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glEnd();
+
+    // Top face (blue)
+    glBegin(GL_QUADS);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glEnd();
+
+    // Bottom face (yellow)
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glEnd();
+
+    // Right face (cyan)
+    glBegin(GL_QUADS);
+    glColor3f(0.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glEnd();
+
+    // Left face (magenta)
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glEnd();
+}
+
+void DrawGLScene()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    
+    glTranslatef(0.0f, 0.0f, -6.0f);
+    glRotatef(angle, 1.0f, 1.0f, 1.0f);
+
+    // Draw rotating cube
+    DrawCube();
+
+    angle += 0.5f;
+    if (angle > 360)
+    {
+        angle -= 360;
+    }
+
+    glutSwapBuffers();
+}
+
+void keyPressed(unsigned char key, int x, int y)
+{
+    if (key == ESCAPE)
+    {
+        glutDestroyWindow(window);
+        exit(0);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(0, 0);
+    window = glutCreateWindow("Rotating Cube");
+    glutDisplayFunc(DrawGLScene);
+    glutIdleFunc(DrawGLScene);
+    glutKeyboardFunc(keyPressed);
+    glEnable(GL_DEPTH_TEST);
+    InitGL(640, 480);
+    glutMainLoop();
+    return 0;
+}
